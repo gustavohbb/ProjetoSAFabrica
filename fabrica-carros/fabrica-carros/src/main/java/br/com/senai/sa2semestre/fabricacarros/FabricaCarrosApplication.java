06efb58fbed70c0e6ab3e9c03f235ec6f17c93fe
Gustavo Borba <107721816+gustavohbb@users.noreply.github.com>
2024-05-20 19:52:50 -0300

    Criação do projeto da SA.

---
+++ fabrica-carros/fabrica-carros/src/main/java/br/com/senai/sa2semestre/fabricacarros/FabricaCarrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai.sa2semestre.fabricacarros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricaCarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabricaCarrosApplication.class, args);
+	}
+
+}
---
+++ fabrica-carros/fabrica-carros/src/test/java/br/com/senai/sa2semestre/fabricacarros/FabricaCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.sa2semestre.fabricacarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabricaCarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
