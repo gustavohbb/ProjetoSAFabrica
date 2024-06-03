446ffdd812abac034e01e0fb003b73374d8b8aa5
Gustavo Borba <107721816+gustavohbb@users.noreply.github.com>
2024-06-03 19:40:48 -0300

    Criação Entidade Estoque

---
+++ fabrica-carros/fabrica-carros/src/main/java/br/com/senai/sa2semestre/fabricacarros/entities/Estoque.java
@@ -0,0 +1,71 @@
+package br.com.senai.sa2semestre.fabricacarros.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+public class Estoque {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "idPecas", referencedColumnName = "idPecas")
+    private Pecas pecas;
+    private int quantidadeDisponivel;
+
+    public Estoque(Long id, Pecas pecas, int quantidadeDisponivel) {
+        this.id = id;
+        this.pecas = pecas;
+        this.quantidadeDisponivel = quantidadeDisponivel;
+    }
+
+    public Estoque() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Pecas getPecas() {
+        return pecas;
+    }
+
+    public void setPecas(Pecas pecas) {
+        this.pecas = pecas;
+    }
+
+    public int getQuantidadeDisponivel() {
+        return quantidadeDisponivel;
+    }
+
+    public void setQuantidadeDisponivel(int quantidadeDisponivel) {
+        this.quantidadeDisponivel = quantidadeDisponivel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Estoque estoque = (Estoque) o;
+        return quantidadeDisponivel == estoque.quantidadeDisponivel && id.equals(estoque.id) && Objects.equals(pecas, estoque.pecas);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, pecas, quantidadeDisponivel);
+    }
+
+    @Override
+    public String toString() {
+        return "Estoque{" +
+                "id=" + id +
+                ", pecas=" + pecas +
+                ", quantidadeDisponivel=" + quantidadeDisponivel +
+                '}';
+    }
+}
