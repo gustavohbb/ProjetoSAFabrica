62eae2fb6f7723ab26c83947aaed178b1c0d62e8
Gustavo Borba <107721816+gustavohbb@users.noreply.github.com>
2024-06-03 18:58:44 -0300

    Criação UsuarioController

---
+++ fabrica-carros/fabrica-carros/src/main/java/br/com/senai/sa2semestre/fabricacarros/controllers/UsuarioController.java
@@ -0,0 +1,54 @@
+package br.com.senai.sa2semestre.fabricacarros.controllers;
+
+import br.com.senai.sa2semestre.fabricacarros.entities.Usuario;
+import br.com.senai.sa2semestre.fabricacarros.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @GetMapping
+    public List<Usuario> getAllUsuarios() {
+        return usuarioRepository.findAll();
+    }
+
+    @GetMapping("/id")
+    public ResponseEntity<Usuario> getUsuarioById(@PathVariable Long id) {
+        Optional<Usuario> usuarioBuscado = usuarioRepository.findById(id);
+        return usuarioBuscado.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Usuario createUsuario(@RequestBody Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @PutMapping
+    public ResponseEntity<Usuario> updateUsuario(@PathVariable Long id, @RequestBody Usuario usuario) {
+        Optional<Usuario> usuarioExistente = usuarioRepository.findById(id);
+        if (usuarioExistente.isPresent()) {
+            usuario.setIdUsuario(id);
+            return ResponseEntity.ok(usuarioRepository.save(usuario));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @DeleteMapping("/id")
+    public ResponseEntity<Void> deleteUsuario(@PathVariable Long id) {
+        Optional<Usuario> usuarioParaDeletar = usuarioRepository.findById(id);
+        if (usuarioParaDeletar.isPresent()) {
+            usuarioRepository.delete(usuarioParaDeletar.get());
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
